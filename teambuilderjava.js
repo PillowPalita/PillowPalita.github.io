21ba030f3db1bbed0b72ca69e77b5305aa41c5b4
Ricardo João Silva <kewry.dev@gmail.com>
2015-01-16 21:57:32 +0000

    First commit

---
+++ teambuilderjava.js
@@ -0,0 +1,20 @@
+var saveLocal = "http//raw.githubusercontent.com/Zarel/Pokemon-Showdown/master/data/";
+var pokedexJSON;
+
+loadData();
+
+function loadData() {
+	$.getJSON(saveLocal + "pokedex.js?callback=?", function(data) {
+		pokedexJSON = data;
+		displayTable();
+	});
+}
+
+function displayTable() {
+
+	$.each(data, function(idx, obj) {
+		$.append("<p>" + obj+"</p>");
+	});
+
+
+}
