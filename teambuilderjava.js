d14c9b2b4a18577e8b83e912dedd3e96537964e8
Ricardo João Silva <kewry.dev@gmail.com>
2015-01-16 23:03:37 +0000

    #

--- teambuilderjava.js
+++ teambuilderjava.js
@@ -2,12 +2,13 @@ var saveLocal = "//cdn.rawgit.com/Zarel/Pokemon-Showdown/master/data/";
 var pokedexJSON;
 
 loadData();
+var exports;
 
 function loadData() {
 	console.warn("Loading...");
 	$.getJSON(saveLocal + "pokedex.js?jsoncallback=?", function(data) {
 		console.warn("Loaded.");
-		pokedexJSON = jQuery.parseJSON(data);
+		pokedexJSON = data;
 		displayTable();
 	});
 }
