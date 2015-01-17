c0797b4956bf9958d8a83ad92af4374ba1ee83c8
Ricardo João Silva <kewry.dev@gmail.com>
2015-01-17 01:35:53 +0000

    #

--- teambuilderjava.js
+++ teambuilderjava.js
@@ -6,7 +6,7 @@ function loadData() {
 	console.warn("Loading...");
 
 $(document).ready(
-	$.get("https://play.pokemonshowdown.com/data/pokedex.js?baa727b9", function(data) {
+	$.get("https://play.pokemonshowdown.com/data/pokedex.js?baa727b9&callback=?", function(data) {
 		console.warn("Loaded. " + data);
 		eval(data);
 		console.warn(exports);
