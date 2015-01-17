8a3c68eeaecda369a269e89c55fc4d81f9fe68fc
Ricardo João Silva <kewry.dev@gmail.com>
2015-01-17 22:02:08 +0000

    #

--- js/teambuilder.js
+++ js/teambuilder.js
@@ -1,21 +1,48 @@
+sortKeys();
 displayTable();
 
 //exports.BattlePokedex
+function sortKeys() {
+	exports.BattlePokedexKeys = [];
+	for(var key in exports.BattlePokedex) {
+		exports.BattlePokedexKeys.push(key);
+	}
+	exports.BattlePokedexKeys.sort();
+}
 
 function displayTable() {
-	$.each(exports.BattlePokedex, function(key, value) {
-		$('body').append('<p style="border:1px dashed cyan">');
+	$.each(exports.BattlePokedexKeys, function(key, value) {
+		
+			drawRow(value, exports.BattlePokedex[value]);
+			//drawTable(key, exports.BattlePokedex);
+		
+	});
+	$('#title').html( 'Pokemons drawn: ' + count);
+}
 
+function drawRow(key, value) {
+	if (value.forme == null) { //remove megas and event pokemon
+		var str = '<p style="border:1px dashed cyan">' + key + ':';
 		$.each(value, function(key, value) {
-			try	{
-				$.each(value, function(key, value) {	
-					$('body').append("{" + key + " " + value + "}");	
-				});
-			} catch (err) {
-					$('body').append("{" + key + " " + value + "}");
+			try {
+				if ($.isArray(value)) {
+					$.each(value, function(key, value) {	str += "[" + key + ":" + value + "]";	});
+				} else if (Object.keys(value).length > 0) {
+					str += "{" + key;
+						try	{
+							$.each(value, function(key, value) {
+								str += "(" + key + ":" + value + ")";
+							});
+						} catch (err) {
+							console.warn(err);
+						}
+
+					str += value + "}";
+				}
+			} catch(err) {
+				str += "(" + key + ":" + value + ")";
 			}
 		});
-
-		$('body').append("</p>");
-	});
+		$('body').append(str + '</p>');
+	}
 }
